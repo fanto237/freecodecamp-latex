2147b01b6890b1b9875a8c7d05227e13b590af30
fanto <58596314+fantoSama@users.noreply.github.com>
2023-05-15 19:36:45 +0200

    initial commit

---
+++ tuto 6 - Packages and Macros/program.cs
@@ -0,0 +1,12 @@
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+
+class Program
+{
+    public static void main(string name)
+    {
+        Console.WriteLine("Hello " + name);
+    }
+}
